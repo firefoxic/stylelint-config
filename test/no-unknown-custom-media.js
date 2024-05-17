841b5ea5d1943b4e06df07dc330de743cb17e4a5
firefoxic <firefoxic.dev@gmail.com>
2024-05-17 13:11:51 -0700

    Disallow unknown custom media queries (#4)
    Resolves #3

---
+++ test/no-unknown-custom-media.js
@@ -0,0 +1,43 @@
+import { testRule } from "../utils/testRule.js"
+
+let rule = `no-unknown-custom-media`
+
+let code = `
+@custom-media --any-valid (width >= 40rem);
+
+@media (--any-valid), (height < 40rem) {}
+
+@media (--another-valid) {}
+
+@custom-media --another-valid (width >= 60rem);
+
+@media (--invalid) {}
+
+@media (--invalid), (height < 40rem) {}
+`
+
+testRule({
+	description: `Custom media queries should be known`,
+	rule,
+	code,
+	expectedWarnings: [
+		{
+			line: 10,
+			column: 9,
+			endLine: 10,
+			endColumn: 18,
+			rule,
+			severity: `error`,
+			text: `Unexpected unknown custom media "--invalid" (${rule})`,
+		},
+		{
+			line: 12,
+			column: 9,
+			endLine: 12,
+			endColumn: 18,
+			rule,
+			severity: `error`,
+			text: `Unexpected unknown custom media "--invalid" (${rule})`,
+		},
+	],
+})
